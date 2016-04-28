6198a59f7658a45c830cd5271e146beb7ba40488
Nika Pona <nikaponens@gmail.com>
2016-04-28 17:17:18 +0200

    Update Nqueens.cc

--- tests/Nqueens.cc
+++ tests/Nqueens.cc
@@ -1,3 +1,4 @@
+// This is a script tp generate an n-queens problem for any n
 #include <iostream>
 #include <math.h>
 #include <fstream>
@@ -5,55 +6,60 @@
 #include <sstream>
 #include <cstring>
 #include <cmath>
+
 using namespace std;
 
-int main() {
-  int result = 0;
-  for(int queens=34; queens < 35; queens++){
+int main( ) {
+  for( int queens = 4 ; queens < 101 ; queens++ ) { 
+    // we want a seperate file for each n = 4...100 with title "nqueens"
+    string name = to_string(queens) + "queens" ;
 
-    string name = to_string(queens)+"queens";
+    int clauses = 0 ; // we want to count clauses to add this info later - could have just found a fomula as in the pigeonhole case
+    int var = queens ; // each variable corresponds to a queen on a column
+    int domain = queens ; // each domain value corresponds to a quuen on a row
+    
+    //open a file
+    ofstream file ;
+    file.open(name) ;
+    
+    // write domain declaration for each variable
 
-    int clauses = 0;
-    int var = queens;
-    int domain = queens;
-    ofstream file;
-    file.open(name);
-
-     for(int i=1; i < var + 1; i++){
-      file<< "d "<<i<< " "<<domain<<endl;
+     for(int i = 1; i < var + 1; i++ ) {
+      file << "d " << i << " " << domain << endl ;
     }
 
-// alldifferent
-    for(int i=0; i < domain; i++){
+// no two queens should be on the same row
+    for ( int i = 0; i < domain ; i ++ ) {
+      for ( int j = 1; j < var + 1 ; j++ ) {
+        for ( int k = 1; k < var + 1 && k != j; k++ ) {
+          file << j << "!=" << i << " " << k << "!=" << i <<" 0" << endl ;
+          clauses++;
+        }
+      }
+    }
+    // commment
+  file << "c diagonals" << endl ;
 
-      for(int j=1; j < var +1; j++){
-          for(int k=1; k < var +1 && k != j; k++){
-        file<<j<<"!="<<i<<" "<<k<<"!="<<i<<" 0"<<endl;
-        clauses++;
+// no two queens should be on the same diagonal:
+  for ( int i = 1 ; i < var + 1 ; i++ ) {
+    for ( int k = 1 ; k < var + 1 && k != i ; k++ ) {
+      for ( int d = 0 ; d < domain ; d++ ) {
+          for( int l = 0 ; l < domain ; l++ ) {
+            if ( i - d == k - l || i + d == k + l ) {
+              file << i<< "!=" << d << " " << k << "!=" << l << " 0" << endl ;
+              clauses++ ;
+            }
+          }
+        }
       }
     }
 
-    }
-file<<"c diagonals"<<endl;
+// adding the problem info - better to do at the beginning
+  file << "p cnf " << var << " " << clauses << endl ;
 
-for(int i=1; i < var+1; i++)
-{
-  for(int k=1; k < var+1 && k!=i; k++)
-    { for(int d=0; d < domain ; d++){
-        for(int l=0; l < domain ; l++)
-      { if (i-d == k-l || i+d == k+l) {
-        file<<i<<"!="<<d<<" "<<k<<"!="<<l<<" 0"<<endl;
-        clauses++;}
-
-    }
+// closing (saving) the file
+    file.close( ) ;
   }
-}
-}
+    return 0 ;
 
-file<<"p cnf "<<var<<" "<<clauses<<endl;
-// cout<<clauses<<endl;
-    file.close();
-    return clauses;
-
-}
 }
