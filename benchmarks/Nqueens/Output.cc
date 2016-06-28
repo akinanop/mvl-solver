b2d21db6aace8f799e8aed8b50fd49ad9a8382db
Nika <nikaponens@gmail.com>
2016-06-28 23:05:29 +0200

    tests

--- benchmarks/Nqueens/Output.cc
+++ benchmarks/Nqueens/Output.cc
@@ -11,13 +11,13 @@ int main( int argc , char** argv ) {
    const char * oldstr ;
    int oldstrlen ;
    ofstream output ;
-   output.open( "results4_60noR" ) ;
+   output.open( "rmk" ) ;
    ifstream infile( argv[ 1 ] ) ;
    if ( infile ) {
       while ( getline( infile , line ) ) {
         oldstr = line.c_str( );
          oldstrlen = strlen( oldstr ) + 1 ;
-        for ( int i=0; i<oldstrlen; i++ )
+        for ( int i=0; i<oldstrlen; i++ ){
             if (oldstr[ i ] == ':'){
               for ( int j = i + 2; j < oldstrlen; j++ ){
                 output << oldstr[ j ];
@@ -27,10 +27,14 @@ int main( int argc , char** argv ) {
             cout << ' ' ;
 
           }
-          output << endl;
-           cout << endl;
-      }
+        }
+
+
+
    }
+   output << endl;
+    cout << endl;
+ }
    infile.close( ) ;
    output.close( ) ;
    return 0 ;
