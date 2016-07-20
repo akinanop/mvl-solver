bce0bb27aacf2b954645b74c346c9db1d9d5cffa
Nika <nikaponens@gmail.com>
2016-07-20 18:52:06 +0200

    bitvec folder

---
+++ bitvec/Variable.cc
@@ -0,0 +1,163 @@
+//**************************************************************
+//
+// Finite Domain Solver
+//
+// File : Variable.cc
+//**************************************************************
+
+//Including Libraries and Header Files
+#include "Variable.h"
+
+using namespace std;
+//**************************************************************
+//Start of Code
+
+//Zero argument constructor
+Variable::Variable()
+{
+	VAR = -1;
+	VAL = -1;
+	DOMAINSIZE = -1;
+	SAT = false;
+	LEVEL = -1;
+	COUNTER = 0;
+	ATOMLEVEL = NULL;
+	ATOMASSIGN = NULL;
+	ATOMCNTPOS = NULL;
+	ATOMCNTNEG = NULL;
+	ATOMRECPOS = NULL;
+	ATOMRECNEG = NULL;
+	VSIDSCOUNTER=NULL;
+	ATOMINDEX = NULL;
+	FLAG = NULL;
+	CLAUSEID = NULL;
+	ATOMWATCH = NULL;
+}
+
+//Two argument constructor
+Variable::Variable(int var, int domain)
+{
+	VAR = var;
+	VAL = -1;
+	DOMAINSIZE = domain;
+	SAT = false;
+	LEVEL = -1;
+	COUNTER = 0;
+	ATOMINDEX = new int[DOMAINSIZE];
+	ATOMWATCH = new int[DOMAINSIZE];
+	ATOMLEVEL = new int[DOMAINSIZE];
+	ATOMASSIGN = new int[DOMAINSIZE];
+	ATOMCNTPOS = new int[DOMAINSIZE];
+	ATOMCNTNEG = new int[DOMAINSIZE];
+	VSIDSCOUNTER = new int[DOMAINSIZE];
+	FLAG = new bool[DOMAINSIZE];
+	CLAUSEID = new int[DOMAINSIZE];
+	ATOMRECPOS = new VARRECORD *[DOMAINSIZE];
+	ATOMRECNEG = new VARRECORD *[DOMAINSIZE];
+	for(int i=0; i<DOMAINSIZE; i++)
+	{ // CHANGED DEFAULT ATOMLEVEL TO -10
+		ATOMLEVEL[i] = -10;
+		ATOMASSIGN[i] = 0;
+		ATOMCNTPOS[i] = 0;
+		ATOMCNTNEG[i] = 0;
+		VSIDSCOUNTER[i] = 0;
+		FLAG[i] = false;
+		CLAUSEID[i] = -10;
+		ATOMRECPOS[i] = NULL;
+		ATOMRECNEG[i] = NULL;
+		ATOMINDEX[i] = -1;
+		ATOMWATCH[i] = 0;
+	}
+}
+
+//Destructor
+Variable::~Variable()
+{
+	delete [] ATOMLEVEL;
+	delete [] ATOMASSIGN;
+	delete [] ATOMCNTPOS;
+	delete [] ATOMCNTNEG;
+	delete [] FLAG;
+	delete [] CLAUSEID;
+	delete [] VSIDSCOUNTER;
+
+	for(int i=0; i<DOMAINSIZE; i++)
+	{
+		VARRECORD * temp = ATOMRECPOS[i];
+		VARRECORD * temp2;
+		while(temp)
+		{
+			temp2 = temp;
+			temp = temp->next;
+			delete temp2;
+		}
+		temp = ATOMRECNEG[i];
+		while(temp)
+		{
+			temp2 = temp;
+			temp = temp->next;
+			delete temp2;
+		}
+		delete ATOMRECPOS[i];
+		delete ATOMRECNEG[i];
+	}
+}
+
+//AddRecord
+void Variable::addRecord ( int c_id, int d_id, bool flag )
+{
+	//local variable
+	VARRECORD * temp = new VARRECORD;
+	temp->c_num = c_id;
+
+	//if memory allocated
+	if(temp)
+	{
+		//if flag = true then its "="; else "!"
+		if(flag)
+		{
+			temp->next = ATOMRECPOS[d_id];
+			ATOMRECPOS[d_id] = temp;
+		}
+		else
+		{
+			temp->next = ATOMRECNEG[d_id];
+			ATOMRECNEG[d_id] = temp;
+		}
+	}
+}
+
+//Print
+void Variable::Print()
+{
+	//print variable name
+	cout<<"Variable = "<<VAR<<endl;
+	for(int i=0; i<DOMAINSIZE; i++)
+	{
+		cout<<"Domain = "<<i<<endl;
+		//print POS
+		cout<<"(POS Records = ";
+		VARRECORD * curr = ATOMRECPOS[i];
+		while(curr)
+		{
+			cout<<curr->c_num<<" ";
+			curr = curr->next;
+		}
+		cout<<")"<<endl;
+		if(curr) delete curr;
+
+		//print NEG
+		cout<<"(NEG Records = ";
+		VARRECORD * curr1 = ATOMRECPOS[i];
+		curr1 = ATOMRECNEG[i];
+		while(curr1)
+		{
+			cout<<curr1->c_num<<" ";
+			curr1 = curr1->next;
+		}
+		cout<<")"<<endl<<endl;
+		if (curr1) delete curr1;
+	}
+}
+//End of Code
+//**************************************************************
